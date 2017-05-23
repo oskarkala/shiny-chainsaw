9ba2cc973fecfbf3c714ffef12e1a73df1bafa51
oskarkala <oskarkala@gmail.com>
2017-05-23 13:36:48 +0300

    Tornado support, logger

--- tornado_app.py
+++ tornado_app.py
@@ -2,12 +2,15 @@ import os
 from tornado.web import Application, FallbackHandler, RequestHandler
 from tornado.wsgi import WSGIContainer
 from tornado.ioloop import IOLoop
+from tornado.log import enable_pretty_logging
 from darksky_api import app
 
 
 APP_PORT = 80
 if 'APP_PORT' in os.environ:
     APP_PORT = os.environ['APP_PORT']
+
+enable_pretty_logging()
 
 
 class MainHandler(RequestHandler):
